d92391c1cb7256ffd393c589c00980ecc71f3b1c
asad124408 <47052705+asad124408@users.noreply.github.com>
2021-09-23 00:45:26 +0600

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,25 +1,25 @@
 $(document).ready(function () {
-    $(".navbar-nav .nav-link").click(function () {
-        $(".navbar-nav .nav-link").removeClass("active");
-        $(this).addClass("active");
-    });
-    $(".filter-button").click(function () {
-        var value = $(this).attr("data-filter");
-
-        if (value == "all") {
-            $(".filter").show("1000");
-        } else {
-            $(".filter")
-                .not("." + value)
-                .hide("3000");
-            $(".filter")
-                .filter("." + value)
-                .show("3000");
-        }
-    });
-
-    if ($(".filter-button").removeClass("active")) {
-        $(this).removeClass("active");
-    }
+  $(".navbar-nav .nav-link").click(function () {
+    $(".navbar-nav .nav-link").removeClass("active");
     $(this).addClass("active");
+  });
+  $(".filter-button").click(function () {
+    var value = $(this).attr("data-filter");
+
+    if (value == "all") {
+      $(".filter").show("1000");
+    } else {
+      $(".filter")
+        .not("." + value)
+        .hide("3000");
+      $(".filter")
+        .filter("." + value)
+        .show("3000");
+    }
+  });
+
+  if ($(".filter-button").removeClass("active")) {
+    $(this).removeClass("active");
+  }
+  $(this).addClass("active");
 });
