b7e8e8b7339f32ab6cff0b76fc6fb7a461094d79
asad124408 <asadullahsarkeriut@gmail.com>
2022-02-09 00:10:24 +0600

    add particle

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Skill from "./components/skills/Skill";
 import Testimonial from "./components/testimonial/Testimonial";
 import Work from "./components/Work/Work";
 import useLocalStorage from "use-local-storage";
+import Particles from "react-tsparticles";
 
 function App() {
   const defaultDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
@@ -20,6 +21,15 @@ function App() {
   const switchTheme = () => {
     const newTheme = theme === "light" ? "dark" : "light";
     setTheme(newTheme);
+  };
+  const particlesInit = (main) => {
+    console.log(main);
+
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+  };
+
+  const particlesLoaded = (container) => {
+    console.log(container);
   };
   useEffect(() => {
     const onScroll = (event) => {
@@ -35,20 +45,107 @@ function App() {
     };
   }, []);
   return (
-    <div data-theme={theme}>
-      <div className="portfolio__app">
-        <Header theme={theme} switchTheme={switchTheme} />
+    <>
+      <Particles
+        style={{ zIndex: 0 }}
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          //   background: {
+          //     color: {
+          //       value: "#0d47a1",
+          //     },
+          //   },
+          fpsLimit: 75,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              bubble: {
+                distance: 400,
+                duration: 2,
+                opacity: 0.8,
+                size: 40,
+              },
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#FEDE00",
+            },
+            links: {
+              color: "#FEDE00",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outMode: "bounce",
+              random: false,
+              speed: 4,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              random: true,
+              value: 5,
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+      <div data-theme={theme} style={{ zIndex: 100, background: "white" }}>
+        <div className="portfolio__app">
+          <div>
+            <Header theme={theme} switchTheme={switchTheme} />
 
-        <Home />
-        <About />
-        <Skill />
-        <Service />
-        <Work />
-        <Testimonial />
-        <Contact />
-        <Footer />
+            <Home />
+            <About />
+            <Skill />
+            <Service />
+            <Work />
+            <Testimonial />
+            <Contact />
+            <Footer />
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
